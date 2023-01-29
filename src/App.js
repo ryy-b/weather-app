dc0bc07967691e89bd75b75fbf6ef91bf5047a87
ryy-b <88695313+ryy-b@users.noreply.github.com>
2023-01-29 17:39:03 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assets/hot.jpg"
+import coldBg from "./assets/cold.jpg"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${coldBg})`}}>
+    <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City." />
+            <button>F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img
+                src="https://openweathermap.org/img/wn/02d@2x.png"
+                alt="weatherIcon"
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 C</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
